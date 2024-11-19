9037f93fcf21cdde3173e00598080b2b54a3e466
junho2408 <bagho1999@gmail.com>
2024-11-19 20:29:24 +0900

    Book Store 프로젝트/ 헤더와 푸터 추가

--- Toy_Projects/book-store/src/App.tsx
+++ Toy_Projects/book-store/src/App.tsx
@@ -1,15 +1,27 @@
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import ThemeSwitcher from "./components/header/ThemeSwitcher";
-import { useContext, useState } from "react";
-import { BookStoreThemeProvider, ThemeContext } from "./context/themeContext";
+import { BookStoreThemeProvider } from "./context/themeContext";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { create } from "domain";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/books",
+    element: <div>돗서 목록</div>
+  }
+])
 
 function App() {
   return (
     <BookStoreThemeProvider>
         <ThemeSwitcher />
         <Layout>
-          <Home />
+          <RouterProvider router={router} />
         </Layout>
     </BookStoreThemeProvider>
   );
--- Toy_Projects/book-store/src/components/layout/Layout.tsx
+++ Toy_Projects/book-store/src/components/layout/Layout.tsx
@@ -1,19 +1,26 @@
-
+import styled from "styled-components";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
 
-interface LayoutProps{
-    children: React.ReactNode;
+interface LayoutProps {
+  children: React.ReactNode;
 }
 
-function Layout({children}: LayoutProps) {
-    return (
-        <div>
-            <Header />
-            <main>{children}</main>
-            <Footer />
-        </div>
-    )
+function Layout({ children }: LayoutProps) {
+  return (
+    <LaytoutStyle>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </LaytoutStyle>
+  );
 }
+
+const LaytoutStyle = styled.main`
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${({ theme }) => theme.layout.width.large};
+  padding: 20px 0;
+`;
 
 export default Layout;
